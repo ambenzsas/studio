f7c6842c86b89e8baf1748ade2b3f8ffd85a7c00
Giobanny Garces <gio.garces.pinzon@gmail.com>
2025-08-24 22:19:17 +0000

    la imagen a la derecha y nuestra historia a la izquierda por favor

--- src/components/landing/about.tsx
+++ src/components/landing/about.tsx
@@ -6,15 +6,6 @@ export default function About() {
       <div className="container mx-auto px-6">
         <div className="flex flex-col items-center gap-12 lg:flex-row lg:gap-16">
           <div className="lg:w-1/2">
-            <Image
-              src="/images/Nuestra Historia.jpg"
-              alt="Taller de AM BENZ con un Mercedes de los 2000"
-              width={600}
-              height={400}
-              className="rounded-lg shadow-lg aspect-[3/2] object-cover"
-            />
-          </div>
-          <div className="lg:w-1/2">
             <h2 className="text-3xl font-extrabold text-foreground sm:text-4xl mb-4">Nuestra Historia</h2>
             <p className="text-muted-foreground mb-6 text-lg">
               AM BENZ S A S fue constituida en febrero del año 2017 y desde entonces, nos hemos dedicado a construir y consolidar la confianza de cada cliente, ofreciéndoles la tranquilidad de saber que su vehículo está en manos de expertos.
@@ -23,6 +14,15 @@ export default function About() {
               <p><strong>Misión:</strong> Ofrecer soluciones integrales en mantenimiento y accesorios de alta calidad para vehículos Mercedes-Benz en Colombia, asegurando la satisfacción y la confianza de nuestros clientes a través de un servicio experto y personalizado.</p>
               <p><strong>Visión:</strong> Ser el principal importador y proveedor de soluciones integrales para vehículos Mercedes-Benz en Colombia, reconocidos por nuestra pasión y el compromiso inquebrantable con la calidad en cada servicio y accesorio.</p>
             </div>
+          </div>
+          <div className="lg:w-1/2">
+            <Image
+              src="/images/Nuestra Historia.jpg"
+              alt="Imagen sobre nosotros"
+              width={600}
+              height={400}
+              className="rounded-lg shadow-lg aspect-[3/2] object-cover"
+            />
           </div>
         </div>
       </div>
